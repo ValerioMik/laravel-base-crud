1c033126214a463f71f18b4c4beea67e42f87ec1
ValerioMik <valerio.avallone@hotmail.com>
2022-01-28 16:23:38 +0100

    migration factory seeder terinate

---
+++ app/Comic.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comic extends Model
+{
+    protected $fillable = [
+        
+    'title',
+    'author',
+    'release_date',
+    'pages',
+
+    ];
+}
